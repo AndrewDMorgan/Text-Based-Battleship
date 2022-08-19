b8bff2eddb21f3c48e001da9cf5ebf25b8d8ee0d
Andrew Morgan <75284724+AndrewDMorgan@users.noreply.github.com>
2022-08-18 23:22:43 -0600

    Added comments
    I added some comments to explain this code a bit more.

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 static int gridX = 10;
 static int gridY = 10;
 
-
+// stores the info on a ship (reducing code and simplifying the code)
 class BattleShip
 {
     public:
@@ -15,7 +15,7 @@ class BattleShip
 
         BattleShip() = default;
         
-
+        // the initzialization of the BattleShip class
         BattleShip(int sizeX_, int sizeY_, int posX_, int posY_, bool* hit_)
         {
             sizeX = sizeX_;
@@ -26,6 +26,7 @@ class BattleShip
             hit = hit_;
         }
     
+        // checks for collition between the ship and any 2D point
         bool PointRectCollition(int x, int y)
         {
             bool xCol = x >= posX && x <= posX + sizeX;
@@ -33,6 +34,7 @@ class BattleShip
             return xCol && yCol;
         }
 
+        // checks for collition between two rectangles
         bool CheckCollition(int sx, int sy, int px, int py)
         {
             bool tlPoint = PointRectCollition(px     , py + sy);
@@ -44,37 +46,43 @@ class BattleShip
 };
 
 
+// renders the baord
 void RenderBaord(BattleShip battleShips[], int hitsX[], int hitsY[], int hits, int missesX[], int missesY[], int misses, int totalShips)
 {
     std::cout << "----------------------" << std::endl;
+    // looping through the layers
     for (int y = 0; y < gridY; y++)
     {
         std::cout << "|";
+        // looping through the rows
         for (int x = 0; x < gridX; x++)
         {
             std::string peice = "  ";
+            // looping through the hits
             for (int i = 0; i < hits; i++)
             {
                 if (hitsX[i] == x && hitsY[i] == y)
                 {
                     peice = "><";
-                    goto end;
+                    goto end;  // ending the search for the correct peice because it was found
                 }
             }
+            // looping through the misses
             for (int i = 0; i < misses; i++)
             {
                 if (missesX[i] == x && missesY[i] == y)
                 {
                     peice = "~~";
-                    goto end;
+                    goto end;  // ending the search for the correct peice because it was found
                 }
             }
+            // looping through the ships
             for (int i = 0; i < totalShips; i++)
             {
                 if (battleShips[i].PointRectCollition(x, y))
                 {
                     peice = "[]";
-                    goto end;
+                    goto end;  // ending the search for the correct peice because it was found
                 }
             }
         end:  // to skip the other loops for preformace if one was found
@@ -88,6 +96,7 @@ void RenderBaord(BattleShip battleShips[], int hitsX[], int hitsY[], int hits, i
 
 int main()
 {
+    // the number of each ship (so its changeable and because I don't know how many of each there are)
     int numCarriers    = 1;
     int numBattleships = 1;
     int numCruisers    = 1;
@@ -97,18 +106,22 @@ int main()
     int shipSizes[5] = {4, 3, 2, 2, 1};  // 1 less than the actuall size btw
     std::string shipNames[5] = {"Carrier", "Battleship", "Cruiser", "Submarine", "Destroyer"};
 
+    // some info on the ships (based on the settings)
     int totalShips = numCarriers + numBattleships + numCruisers + numSubmarines + numDestroyers;
     int numShips[5] = {numCarriers, numBattleships, numCruisers, numSubmarines, numDestroyers};
 
     BattleShip ships1[5];
 
+    // an empty list of ints (for use when placing ships)
     int emptyIntList[0];
 
+    // placing ships on the board
     float currentType = 0;
     for (int i = 0; i < totalShips; i++)
     {
         int shipIndex = currentType;
 
+        // basic veriables for the ship
         bool hit[5];
         int xPos = 0;
         int yPos = 0;
@@ -117,12 +130,15 @@ int main()
         bool placed = false;
         int xSize = shipSizes[shipIndex];
         ships1[i] = BattleShip(xSize, ySize, xPos, yPos, hit);
+        // rendering the board (so you can see where the ship is)
         RenderBaord(ships1, emptyIntList, emptyIntList, 0, emptyIntList, emptyIntList, 0, i + 1);
         while (!placed)
         {
+            // printing out the basic information
             std::cout << shipNames[shipIndex] << "\nSize: " << shipSizes[shipIndex] << "\np to place\nr to rotate\nwasd to move\n > ";
             std::cin >> input;
             
+            // interpreting the inputed information
             if (input == "p") placed = true;  // loop i-1 times and check for collition before placing pices
             else if (input == "w") yPos--;
             else if (input == "a") xPos--;
@@ -130,6 +146,7 @@ int main()
             else if (input == "d") xPos++;
             else if (input == "r")
             {
+                // checking the current rotation and changing the rotation from there
                 if (ySize == 0)
                 {
                     xSize = 0;
@@ -142,12 +159,14 @@ int main()
                 }
             }
 
+            // creating the new battleship
             ships1[i] = BattleShip(xSize, ySize, xPos, yPos, hit);
 
+            // rendering the new setup
             RenderBaord(ships1, emptyIntList, emptyIntList, 0, emptyIntList, emptyIntList, 0, i + 1);
         }
 
-        currentType += 1./(float)numShips[shipIndex];
+        currentType += 1./(float)numShips[shipIndex];  // incrementing the type index
     }
 
     return 0;
